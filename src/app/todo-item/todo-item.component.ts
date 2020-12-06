31a0d4139e6c91eea099b96d2c4e35ba544a57ed
divin <angapaydivin@gmail.com>
2020-12-06 13:25:59 +0000

    ajout du module FontAwesome et implémentation des fonctions undo et redo

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,15 +1,51 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, Input, ViewChild, ElementRef} from '@angular/core';
+import { TodoItemData } from '../dataTypes/TodoItemData';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() private data: TodoItemData;
+  @ViewChild("newTextInput", {static: false}) private inputLabel: ElementRef;
+  private _editionMode: boolean = false;
+  
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
   }
 
+  get label() : string {
+    return this.data.label
+  }
+
+  set label(newLabel: string) {
+    newLabel !== this.data.label ? this.todoService.setItemsLabel(newLabel, this.data) : '';
+    this._editionMode = false;
+  }
+
+  get isDone() : boolean {
+    return this.data.isDone;
+  }
+
+  set isDone(done: boolean) {
+    this.todoService.setItemsDone(done, this.data);
+  }
+
+  get editionMode(): boolean {
+    return this._editionMode;
+  }
+
+  set editionMode(e: boolean) {
+    this._editionMode = e;
+    requestAnimationFrame(() => this.inputLabel.nativeElement.focus());
+  }
+
+  removeItem() {
+    this.todoService.removeItems(this.data);
+  }
 }
