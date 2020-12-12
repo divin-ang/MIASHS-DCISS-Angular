a9c6d96029f648d1d54d9c2333febd0ed304a88d
divin <angapaydivin@gmail.com>
2020-12-12 23:18:03 +0000

    ajout du service de reconnaissance vocale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
-import {TodoService} from './todo.service';
-import {FormsModule} from '@angular/forms';
+import { TodoService } from './todo.service';
+import { SpeechRecognitionService } from './speech-recognition.service';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
@@ -16,9 +18,11 @@ import {FormsModule} from '@angular/forms';
     TodoItemComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, 
+    FormsModule, 
+    FontAwesomeModule
   ],
-  providers: [TodoService],
+  providers: [TodoService, SpeechRecognitionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
